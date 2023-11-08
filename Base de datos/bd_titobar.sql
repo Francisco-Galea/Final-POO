97fba239db41cc3df23ea93637bedfe70f2ee716
Francisco Galea <franciscogalea@gmail.com>
2023-11-08 16:37:31 -0300

    Changes added

--- Base de datos/bd_titobar.sql
+++ Base de datos/bd_titobar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-11-2023 a las 01:41:49
+-- Tiempo de generación: 08-11-2023 a las 20:36:34
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -41,7 +41,8 @@ CREATE TABLE `mercaderia` (
 --
 
 INSERT INTO `mercaderia` (`ID`, `Nombre`, `Categoria`, `Costo`, `Precio`, `Stock`) VALUES
-(35, 'Milanesa de pollo', 'Carnes', 1500.00, 3300.00, 15);
+(36, 'Hamburguesa VEGANA', 'Carnes', 850.55, 3600.00, 100),
+(37, 'Ensalda rusa', 'Vegetales', 250.85, 600.79, 25);
 
 -- --------------------------------------------------------
 
@@ -60,29 +61,7 @@ CREATE TABLE `mesa` (
 --
 
 INSERT INTO `mesa` (`idMesa`, `numeroMesa`, `activo`) VALUES
-(1, 1, 1),
-(2, 1, 1),
-(3, 2, 1),
-(4, 3, 1),
-(6, 1, 1),
-(7, 2, 1),
-(8, 3, 1),
-(9, 1, 1),
-(14, 1, 1),
-(15, 2, 1),
-(16, 1, 1),
-(17, 2, 1),
-(18, 1, 1),
-(19, 1, 1),
-(20, 2, 1),
-(21, 1, 1),
-(28, 2, 1),
-(30, 2, 1),
-(33, 3, 1),
-(36, 2, 1),
-(38, 2, 1),
-(39, 1, 1),
-(42, 4, 1);
+(5, 1, 1);
 
 --
 -- Índices para tablas volcadas
@@ -108,13 +87,13 @@ ALTER TABLE `mesa`
 -- AUTO_INCREMENT de la tabla `mercaderia`
 --
 ALTER TABLE `mercaderia`
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT de la tabla `mesa`
 --
 ALTER TABLE `mesa`
-  MODIFY `idMesa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `idMesa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
